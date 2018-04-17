512f0b5cbdbf2f3935806d9706b1161d57f6ed07
Panji Y. Wiwaha <panjiyudasetya@gmail.com>
2018-04-17 15:48:15 +0700

    wired up snippet view up

--- drf_tutorial/urls.py
+++ drf_tutorial/urls.py
@@ -17,13 +17,16 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from quickstart import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+ROUTER = routers.DefaultRouter()
+# Wire quickstart views up
+ROUTER.register(r'users', views.UserViewSet)
+ROUTER.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^', include(ROUTER.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # Wire snippet views up
+    url(r'^', include('snippets.urls')),
 ]
